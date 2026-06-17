f819d35270755a933f64cdd2731c92c33182bad0
PetitCh <josepetitch@gmail.com>
2026-06-17 15:03:55 -0400

    Api sencilla

---
+++ MÓDULO 4/clase 3/api.py
@@ -0,0 +1,8 @@
+from flask import jsonify, Flask
+#"tu puta madre broenkjkejkejkejkejkejekjeke"  
+app = Flask(__name__)
+
+my_items = {
+    "id": 1, "item": "Nintendo2ds", "price": "idk",
+    "id": 2, "item": "PC CULERA", "price": "neither xd"
+}
